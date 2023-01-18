0dd4f10f7f4794df96af5323dc3eb2e669b6a889
Ahmed Sufyan <sufyandaredevil@gmail.com>
2023-01-18 20:28:39 +0530

    removed unassigned port numbers, removed service names that is not assigned with a specific port, added argparse module, change json structure for O(1) searching

--- spd.py
+++ spd.py
@@ -1,26 +1,24 @@
-import sys
 import json
+import argparse
 from prettytable import PrettyTable
 
-x = PrettyTable()
-x.field_names = ["Port Number", "Service Name", "Description"]
+with open("spd.json", "r") as f:
+    new_data = json.load(f)
 
-if len(sys.argv) != 2:
-    print("Usage: python spd.py <port_number>")
+parser = argparse.ArgumentParser(description="Retrieve service name and description for a given port number. Only Well-known & Registered ports are present")
+parser.add_argument("port_number", type=int, help="The port number to retrieve information for")
+args = parser.parse_args()
+port_number = args.port_number
+
+if not (0 <= port_number <= 49151):
+    parser.error("Invalid port number, must be between 0 and 49151")
+
+if str(port_number) in new_data:
+    table = PrettyTable()
+    table.field_names = ["Port Number", "Service Name", "Description"]
+
+    for item in new_data[str(port_number)]:
+        table.add_row([port_number, item["1"], item["2"]])
+    print(table)
 else:
-    port = sys.argv[1]
-    mark = None
-
-    with open('spd.json') as json_file:
-        spds = json.load(json_file)
-
-        for i in range(len(spds)):
-            if mark != None and spds[i]["0"] > mark:
-                print(x)
-                break
-            if str(spds[i]["0"]) == port:
-                x.add_row([spds[i]["0"], spds[i]["1"], spds[i]["2"]])
-                mark = int(port)
-
-    if mark == None:
-        print('No service names found for ', port)
+    print("{} not found. (Is that a dynamic port number?)".format(port_number))
